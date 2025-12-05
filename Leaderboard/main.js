4a92b475e0cc3dc1c0b22bfa8cde70d79b3569b9
omar salah <114373468+omarsalaheldieen@users.noreply.github.com>
2025-12-05 14:12:53 +0200

    Update main.js

--- Leaderboard/main.js
+++ Leaderboard/main.js
@@ -16,7 +16,7 @@ function createDefaultScoreboard() {
 
 createDefaultScoreboard();
 
-socket.on("connection", (data) => {
+socket.on("connection:Vodafone Booth", (data) => {
     let scores = JSON.parse(data);
     console.log("Initial scores:", scores);
 
@@ -42,7 +42,7 @@ socket.on("connection", (data) => {
     }
 });
 
-socket.on("scoreUpdate", (data) => {
+socket.on("scoreUpdate:Vodafone Booth", (data) => {
     console.log("Score update received:");
     
     let scores = JSON.parse(data);
